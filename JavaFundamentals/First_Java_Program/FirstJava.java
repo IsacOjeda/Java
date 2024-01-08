3b02047a274a502ffdb048f7e61894661000a246
Isac <isacojeda717@gmail.com>
2024-01-08 15:32:16 -0800

    First_Java_Practice_Assignment

---
+++ JavaFundamentals/First_Java_Program/FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Isac");
+        System.out.println("I am 20 years old");
+        System.out.println("My hometown is Bakersfield, CA");
+    }
+
+}
